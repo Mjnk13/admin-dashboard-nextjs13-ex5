5f0813feff25d6a5a3c4b685d8658c555371c16d
Minh_Cuteee <nhatm490@gmail.com>
2023-08-24 17:36:36 +0700

    initial

---
+++ src/app/_component/navbar/NavBar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const NavBar = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon text-white"></span>
+                </button>
+                <Link href={"/"} className="navbar-brand fw-bold text-white">Dashboard</Link>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
+                    <ul className="m-0 navbar-nav">
+                        <li className="nav-item">
+                            <Link className="text-decoration-none text-info" href={"/"}>Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
